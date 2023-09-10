da04e0ed97778f9bbe56fe8a8d052ed5f71e5cd4
Mirau Pavel <miraupavel@gmail.com>
2023-09-10 17:10:59 +0700

    two commit

--- src/main/java/ru/netology/qamid/Radio.java
+++ src/main/java/ru/netology/qamid/Radio.java
@@ -3,21 +3,15 @@ package ru.netology.qamid;
 public class Radio {
     private int currentVolume;
     private int currentChannel;
-    private final int maxChannel;
-    private final int maxVolume;
-    private boolean on;
+    private int maxChannel;
 
     public Radio() {
-        this.maxChannel = 9;
-        this.maxVolume = 100;
     }
 
-    public Radio(int currentVolume, int currentChannel, int maxChannel, int maxVolume, boolean on) {
+    public Radio(int currentVolume, int currentChannel, int maxChannel) {
         this.currentVolume = currentVolume;
         this.currentChannel = currentChannel;
         this.maxChannel = maxChannel;
-        this.maxVolume = maxVolume;
-        this.on = on;
     }
 
     public int getCurrentVolume() {
@@ -40,86 +34,9 @@ public class Radio {
         return maxChannel;
     }
 
-    public int getMaxVolume() {
-        return maxVolume;
+    public void setMaxChannel(int maxChannel) {
+        this.maxChannel = maxChannel;
     }
-
-    public boolean isOn() {
-        return on;
-    }
-
-    public void setOn(boolean on) {
-        this.on = on;
-    }
-    //    public Radio() {
-//        this.maxChannel = 9;
-//        this.maxVolume = 100;
-//    }
-//
-//    public Radio(int channel, int volume) {
-//        this.maxChannel = channel - 1;
-//        this.maxVolume = volume - 1;
-//    }
-//
-//    public int getCurrentChannel() {
-//        return currentChannel;
-//    }
-//
-//    public void setCurrentChannel(int currentChannel) {
-//        if (currentChannel < 0) {
-//            return;
-//        }
-//        if (currentChannel > maxChannel) {
-//            return;
-//        }
-//        this.currentChannel = currentChannel;
-//    }
-//
-//    public void next() {
-//        if (currentChannel != maxChannel) {
-//            currentChannel++;
-//        } else {
-//            currentChannel = 0;
-//        }
-//    }
-//
-//    public void prev() {
-//        if (currentChannel != maxChannel) {
-//            currentChannel--;
-//        }
-//        if (currentChannel < 0) {
-//            currentChannel = maxChannel;
-//        }
-//    }
-//
-//    public int getCurrentVolume() {
-//        return currentVolume;
-//    }
-//
-//    public void setCurrentVolume(int currentVolume) {
-//        if (currentVolume >= maxVolume)
-//            currentVolume = 100;
-//        if (currentVolume <= 0) {
-//            currentVolume = 0;
-//        }
-//        this.currentVolume = currentVolume;
-//    }
-//
-//    public void nextVolume() {
-//        if (currentVolume < maxVolume) {
-//            currentVolume++;
-//        }
-//        if (currentVolume >= maxVolume) {
-//            currentVolume = maxVolume;
-//        }
-//    }
-//
-//    public void prevVolume() {
-//        if (currentVolume <= maxVolume) {
-//            currentVolume--;
-//        }
-//        if (currentVolume <= 0) {
-//            currentVolume = 0;
-//        }
-//    }
 }
+
+
--- src/test/java/ru/netology/qamid/RadioTest.java
+++ src/test/java/ru/netology/qamid/RadioTest.java
@@ -2,22 +2,9 @@ package ru.netology.qamid;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+
 
 class RadioTest {
-//    @ParameterizedTest
-//    @CsvSource({
-//            "6,4,4"
-//    }) // Работает
-
-    //    @CsvFileSource(files = "src/test/java/resources/setChannel.csv")
-//    public void currentChannel(int getCurrentChannel, int currentChannel, int expected) {
-//        Radio radio = new Radio(9);
-//        int actual = radio.getCurrentChannel(getCurrentChannel, currentChannel, expected);
-//        Assertions.assertEquals(expected, actual);
-//    }
 
     @Test
     public void currentChannelNoLimit() {
@@ -63,124 +50,124 @@ class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void nextChannel() {
-        Radio radio = new Radio();
-
-        radio.setCurrentChannel(9);
-        radio.next();
-        int expected = 0;
-        int actual = radio.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextChannelTest() {
-        Radio radio = new Radio();
-
-        radio.setCurrentChannel(8);
-        radio.next();
-        int expected = 9;
-        int actual = radio.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextChannelUp() {
-        Radio radio = new Radio();
-
-        radio.setCurrentChannel(5);
-        radio.next();
-        int expected = 6;
-        int actual = radio.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void prevChannel() {
-        Radio radio = new Radio();
-
-        radio.setCurrentChannel(0);
-        radio.prev();
-        int expected = 9;
-        int actual = radio.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void prevChannelUnlimited() {
-        Radio radio = new Radio();
-
-        radio.setCurrentChannel(-1);
-        radio.prev();
-        int expected = 9;
-        int actual = radio.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextVolumeLevel() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(100);
-        radio.nextVolume();
-        int expected = 100;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextVolumeLevelUp() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(99);
-        radio.nextVolume();
-        int expected = 100;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextVolumeLevelUpLimited() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(101);
-        radio.nextVolume();
-        int expected = 100;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextVolumeLevelIncrease() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(0);
-        radio.nextVolume();
-        int expected = 1;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void prevVolumeLevel() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(0);
-        radio.prevVolume();
-        int expected = 0;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void prevVolumeLevelDecrease() {
-        Radio radio = new Radio();
-
-        radio.setCurrentVolume(100);
-        radio.prevVolume();
-        int expected = 99;
-        int actual = radio.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void nextChannel() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentChannel(9);
+//        radio.next();
+//        int expected = 0;
+//        int actual = radio.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nextChannelTest() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentChannel(8);
+//        radio.next();
+//        int expected = 9;
+//        int actual = radio.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nextChannelUp() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentChannel(5);
+//        radio.next();
+//        int expected = 6;
+//        int actual = radio.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void prevChannel() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentChannel(0);
+//        radio.prev();
+//        int expected = 9;
+//        int actual = radio.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void prevChannelUnlimited() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentChannel(-1);
+//        radio.prev();
+//        int expected = 9;
+//        int actual = radio.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nextVolumeLevel() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentVolume(100);
+//        radio.nextVolume();
+//        int expected = 100;
+//        int actual = radio.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nextVolumeLevelUp() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentVolume(99);
+//        radio.nextVolume();
+//        int expected = 100;
+//        int actual = radio.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nextVolumeLevelUpLimited() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentVolume(101);
+//        radio.nextVolume();
+//        int expected = 100;
+//        int actual = radio.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nextVolumeLevelIncrease() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentVolume(0);
+//        radio.nextVolume();
+//        int expected = 1;
+//        int actual = radio.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void prevVolumeLevel() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentVolume(0);
+//        radio.prevVolume();
+//        int expected = 0;
+//        int actual = radio.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void prevVolumeLevelDecrease() {
+//        Radio radio = new Radio();
+//
+//        radio.setCurrentVolume(100);
+//        radio.prevVolume();
+//        int expected = 99;
+//        int actual = radio.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
